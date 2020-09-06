93013c563f6a5946f3fcd611aeff2f295cd778ca
Mahmoud Muhammed <mandoooman@gmail.com>
2020-09-06 18:22:59 +0200

    last changed

--- resources/lang/ar/sidebar.php
+++ resources/lang/ar/sidebar.php
@@ -9,13 +9,10 @@ return [
      'admin.all'       => 'كافة المديرين',
      'admin.create'    => 'اضافة مدير جديد',
      'admin.show'      => 'الملف الشخصى',
-
      'customer.index'   => 'كل العملاء' ,
      'customer.create'  => 'اضافة عميل جديد' ,
      'customer.all'     => 'كافة العملاء',
-
      'role.index'     => 'الصلاحيات',
      'role.create'    => 'اضافة مجموعة جديدة',
      'role.all'       => 'كافة الصلاحيات',
-
 ];
--- resources/lang/en/sidebar.php
+++ resources/lang/en/sidebar.php
@@ -6,5 +6,14 @@ return [
      'dashbord.index'  => 'dashbord',
      'admin.index'     => 'admins',
      'admin.create'    => 'create new admin',
-   
+     'auth.login'      => 'تسجيل الدخول',
+     'admin.all'       => 'كافة المديرين',
+     'admin.show'      => 'الملف الشخصى',
+     'customer.index'   => 'كل العملاء' ,
+     'customer.create'  => 'اضافة عميل جديد' ,
+     'customer.all'     => 'كافة العملاء',
+     'role.index'     => 'الصلاحيات',
+     'role.create'    => 'اضافة مجموعة جديدة',
+     'role.all'       => 'كافة الصلاحيات',
+
 ];

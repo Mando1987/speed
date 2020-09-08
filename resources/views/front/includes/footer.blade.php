5643bbefdaea4182dc53fde1e10474e88b664c86
AMR-WAHBA <70824114+AMR-WAHBA@users.noreply.github.com>
2020-09-08 21:09:57 +0200

    Update footer.blade.php

--- resources/views/front/includes/footer.blade.php
+++ resources/views/front/includes/footer.blade.php
@@ -6,7 +6,7 @@
                 <p>@lang('front.footer_title')</p>
             </div>
             <div class="col-md-4">
-                <h3>لينكات </h3>
+                <h3>لينكات</h3>
                 <div class="border-color"></div>
                 <ul class="list-mark">
                     <li><a href="index.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_home')</a></li>
@@ -24,7 +24,7 @@
             </div>
             <div class="col-md-4">
 
-                <h3>خدماتنا </h3>
+                <h3>خدماتنا</h3>
                 <div class="border-color"></div>
                 <ul class="list-mark">
                     <li><a href="services/Same-day-deliveryn.html"><i

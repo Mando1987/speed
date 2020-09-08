236dd0e5e17b05f7ea1986e5bee8d0b4e4820ff9
Mahmoud Muhammed <mandoooman@gmail.com>
2020-09-08 02:00:09 +0200

    translate fotter file

--- resources/views/front/includes/footer.blade.php
+++ resources/views/front/includes/footer.blade.php
@@ -3,19 +3,22 @@
         <div class="row">
             <div class="col-md-4 footer-right">
                 <img class="img-responsive" src="{{asset('front/uploads/logo/1491769630.png')}}" alt="logo" />
-                <p>ليه تشحن مع سبيد لأننا الأفضل .. الأسرع .. والأسهل</p>
+                <p>@lang('front.fotter_title')</p>
             </div>
             <div class="col-md-4">
                 <h3>لينكات </h3>
                 <div class="border-color"></div>
                 <ul class="list-mark">
-                    <li><a href="index.html"><i class=" fa fa-angle-left"></i>الرئيسية</a></li>
-                    <li><a href="prices.html"><i class=" fa fa-angle-left"></i>الأسعار</a></li>
-                    <li><a href="makeorders.html"><i class=" fa fa-angle-left"></i>طلب شحن</a></li>
-                    <li><a href="about.html"><i class=" fa fa-angle-left"></i>من نحن</a></li>
-                    <li><a href="contact.html"><i class=" fa fa-angle-left"></i>اتصل بنا</a></li>
-                    <li><a href="terms.html"><i class=" fa fa-angle-left"></i>شروط الأستخدام</a></li>
-                    <li><a href="join.html"><i class=" fa fa-angle-left"></i>أنضم لفريق العمل</a></li>
+                    <li><a href="index.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_home')</a></li>
+                    <li><a href="prices.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_prices')</a></li>
+                    <li><a href="makeorders.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_ordercharge') </a>
+                    </li>
+                    <li><a href="about.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_uswe')</a></li>
+                    <li><a href="contact.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_callme')</a></li>
+                    <li><a href="terms.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_licinse')</a>
+                    </li>
+                    <li><a href="join.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_jointoteam')</a>
+                    </li>
                 </ul>
             </div>
             <div class="col-md-4">
@@ -23,25 +26,21 @@
                 <h3>خدماتنا </h3>
                 <div class="border-color"></div>
                 <ul class="list-mark">
-                    <li><a href="services/Same-day-deliveryn.html"><i
-                                class=" fa fa-angle-left"></i>توصيل نفس اليوم</a></li>
-                    <li><a href="services/money-collection.html"><i class=" fa fa-angle-left"></i>تحصيل
-                            الاموال</a></li>
-                    <li><a href="services/internal-shipping.html"><i class=" fa fa-angle-left"></i>شحن
-                            داخلي</a></li>
-                    <li><a href="services/fbs.html"><i class=" fa fa-angle-left"></i>تشحن وتغلف من قبل
-                            سبيد</a></li>
-                    <li><a href="services/speed-app.html"><i class=" fa fa-angle-left"></i>Speed App</a>
+                    <li><a href="services/Same-day-deliveryn.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_sendInTheSameDay')</a>
                     </li>
-                    <li><a href="services/mailing.html"><i class=" fa fa-angle-left"></i>توزيع المراسلات
-                            الكمية</a></li>
-                    <li><a href="services/documents.html"><i class=" fa fa-angle-left"></i>خدمة توصيل
-                            المستندات</a></li>
+                    <li><a href="services/money-collection.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_licinse')</a></li>
+                    <li><a href="services/internal-shipping.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_chargeIN')</a></li>
+                    <li><a href="services/fbs.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_chargeinspeed')</a></li>
+                    <li><a href="services/speed-app.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_speedApp')</a>
+                    </li>
+                    <li><a href="services/mailing.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_sendQuntitysenders')</a></li>
+                    <li><a href="services/documents.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_documentSendService')</a></li>
                 </ul>
             </div>
         </div>
         <div class="row">
-            <div class="copyright">جميع الحقوق محفوظة لشركة سبيد للشحن السريع © 2020</div>
+            <div class="copyright">@lang('front.fotter_copyright')</div>
         </div>
     </div>
 </footer>
+@lang('front.footer_')

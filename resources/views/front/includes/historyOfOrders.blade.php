3a320dd5df79f2cb127c304a8e2890c5cbfde4d7
AMR-WAHBA <70824114+AMR-WAHBA@users.noreply.github.com>
2020-09-08 21:36:27 +0200

    Update historyOfOrders.blade.php

--- resources/views/front/includes/historyOfOrders.blade.php
+++ resources/views/front/includes/historyOfOrders.blade.php
@@ -13,29 +13,25 @@
                       <span class="list-progress__label">الطلبات التى قمنا بها</span>
                   </li>
                   <li class="list-progress__item"><i class="icon fa fa-users"></i>
-                      <div class="list-progress__info"><span data-percent="9843 " class="chart"><span
-                                  class="percent"></span></span>
+                      <div class="list-progress__info"><span data-percent="9843 " class="chart"><span class="percent"></span></span>
                       </div>
                       <div class="clearfix"></div>
                       <span class="list-progress__label">عملائنا</span>
                   </li>
                   <li class="list-progress__item"><i class="icon fa fa-home"></i>
-                      <div class="list-progress__info"><span data-percent="2" class="chart"><span
-                                  class="percent"></span></span>
+                      <div class="list-progress__info"><span data-percent="2" class="chart"><span class="percent"></span></span>
                       </div>
                       <div class="clearfix"></div>
                       <span class="list-progress__label">فروعنا</span>
                   </li>
                   <li class="list-progress__item"><i class="icon fa fa-comments-o"></i>
-                      <div class="list-progress__info"><span data-percent="469357 "
-                              class="chart"><span class="percent"></span></span>
+                      <div class="list-progress__info"><span data-percent="469357 " class="chart"><span class="percent"></span></span>
                       </div>
                       <div class="clearfix"></div>
                       <span class="list-progress__label">الطلبات من خلال الموقع</span>
                   </li>
                   <li class="list-progress__item"><i class="icon fa fa-bar-chart"></i>
-                      <div class="list-progress__info"><span data-percent="950" class="chart"><span
-                                  class="percent"></span></span>
+                      <div class="list-progress__info"><span data-percent="950" class="chart"><span class="percent"></span></span>
                       </div>
                       <div class="clearfix"></div>
                       <span class="list-progress__label">طلبات هذا الشهر</span>

cb58e2ab5bb3081096bffced76f43474b3917c44
AMR-WAHBA <70824114+AMR-WAHBA@users.noreply.github.com>
2020-09-08 21:40:08 +0200

    Update footer.blade.php

--- resources/views/front/includes/footer.blade.php
+++ resources/views/front/includes/footer.blade.php
@@ -12,7 +12,7 @@
                     <li><a href="index.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_home')</a></li>
                     <li><a href="prices.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_prices')</a></li>
                     <li><a href="makeorders.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_ordercharge')
-                        </a>
+                    </a>
                     </li>
                     <li><a href="about.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_uswe')</a></li>
                     <li><a href="contact.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_callme')</a></li>
@@ -23,26 +23,16 @@
                 </ul>
             </div>
             <div class="col-md-4">
-
                 <h3>خدماتنا</h3>
                 <div class="border-color"></div>
                 <ul class="list-mark">
-                    <li><a href="services/Same-day-deliveryn.html"><i
-                                class=" fa fa-angle-left"></i>@lang('front.footer_sendInTheSameDay')</a>
-                    </li>
-                    <li><a href="services/money-collection.html"><i
-                                class=" fa fa-angle-left"></i>@lang('front.footer_licinse')</a></li>
-                    <li><a href="services/internal-shipping.html"><i
-                                class=" fa fa-angle-left"></i>@lang('front.footer_chargeIN')</a></li>
-                    <li><a href="services/fbs.html"><i
-                                class=" fa fa-angle-left"></i>@lang('front.footer_chargeinspeed')</a></li>
-                    <li><a href="services/speed-app.html"><i
-                                class=" fa fa-angle-left"></i>@lang('front.footer_speedApp')</a>
-                    </li>
-                    <li><a href="services/mailing.html"><i
-                                class=" fa fa-angle-left"></i>@lang('front.footer_sendQuntitysenders')</a></li>
-                    <li><a href="services/documents.html"><i
-                                class=" fa fa-angle-left"></i>@lang('front.footer_documentSendService')</a></li>
+                    <li><a href="services/Same-day-deliveryn.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_sendInTheSameDay')</a></li>
+                    <li><a href="services/money-collection.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_licinse')</a></li>
+                    <li><a href="services/internal-shipping.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_chargeIN')</a></li>
+                    <li><a href="services/fbs.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_chargeinspeed')</a></li>
+                    <li><a href="services/speed-app.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_speedApp')</a></li>
+                    <li><a href="services/mailing.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_sendQuntitysenders')</a></li>
+                    <li><a href="services/documents.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_documentSendService')</a></li>
                 </ul>
             </div>
         </div>

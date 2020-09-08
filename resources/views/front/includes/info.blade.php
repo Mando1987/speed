6e4ba52d081cda3471d1c52b4c91684a2db4730b
AMR-WAHBA <70824114+AMR-WAHBA@users.noreply.github.com>
2020-09-08 21:35:04 +0200

    Update info.blade.php

--- resources/views/front/includes/info.blade.php
+++ resources/views/front/includes/info.blade.php
@@ -5,26 +5,18 @@
             <div class="row">
                 <div class="col-lg-12 text-center col-sm-4 col-xs-12">
                     <ul class="social-links">
-                        <li class="social-links__item"><a href="https://www.twitter.com" class="social-links__link"><i
-                                    class="icon fa fa-twitter"></i></a> </li>
-                        <li class="social-links__item"><a href="https://www.facebook.com/Speed.courier.service"
-                                class="social-links__link"><i class="icon fa fa-facebook"></i></a> </li>
-                        <li class="social-links__item"><a href="#" class="social-links__link"><i
-                                    class="icon fa fa-google-plus"></i></a> </li>
-                        <li class="social-links__item"><a href="https://www.youtube.com/" class="social-links__link"><i
-                                    class="icon fa fa-youtube"></i></a> </li>
-                        <li class="social-links__item"><a href="https://www.instgram.com/" class="social-links__link"><i
-                                    class="icon fa fa-instagram"></i></a>
+                        <li class="social-links__item"><a href="https://www.twitter.com" class="social-links__link"><i class="icon fa fa-twitter"></i></a> </li>
+                        <li class="social-links__item"><a href="https://www.facebook.com/Speed.courier.service"class="social-links__link"><i class="icon fa fa-facebook"></i></a> </li>
+                        <li class="social-links__item"><a href="#" class="social-links__link"><i class="icon fa fa-google-plus"></i></a> </li>
+                        <li class="social-links__item"><a href="https://www.youtube.com/" class="social-links__link"><i class="icon fa fa-youtube"></i></a> </li>
+                        <li class="social-links__item"><a href="https://www.instgram.com/" class="social-links__link"><i class="icon fa fa-instagram"></i></a>
                         </li>
                     </ul>
                 </div>
-
                 <div class="col-md-8 col-md-offset-2">
                     <div class="about-footer text-center">
-                        <address class="col-xs-12 col-sm-4 mt-2">أرض اللواء - العجوزه<i class="fa fa-map-marker "></i>
-                        </address>
-                        <div class="email col-xs-12 col-sm-4 mt-2">sales@speedshippings.com<i
-                                class="fa fa-envelope"></i>
+                        <address class="col-xs-12 col-sm-4 mt-2">أرض اللواء - العجوزه<i class="fa fa-map-marker "></i></address>
+                        <div class="email col-xs-12 col-sm-4 mt-2">sales@speedshippings.com<i class="fa fa-envelope"></i>
                         </div>
                         <div class="mobile col-xs-12 col-sm-4 mt-2">01098026159 <i class="fa fa-phone"></i>
 

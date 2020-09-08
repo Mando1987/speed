a2cb56c387b5ac7aaf4799920ce88aef0bd5afac
AMR-WAHBA <70824114+AMR-WAHBA@users.noreply.github.com>
2020-09-08 21:39:01 +0200

    Update header.blade.php

--- resources/views/front/includes/header.blade.php
+++ resources/views/front/includes/header.blade.php
@@ -8,28 +8,17 @@
                 <div class="col-sm-3">
                     <div class="header__social text-center">
                         <ul class="social-links text-center">
-                            <li class="social-links__item"><a href="https://www.twitter.com"
-                                    class="social-links__link"><i class="icon fa fa-twitter"></i></a>
-                            </li>
-                            <li class="social-links__item"><a
-                                    href="https://www.facebook.com/Speed.courier.service"
-                                    class="social-links__link"><i class="icon fa fa-facebook"></i></a>
-                            </li>
-                            <li class="social-links__item"><a href="#" class="social-links__link"><i
-                                        class="icon fa fa-google-plus"></i></a> </li>
-                            <li class="social-links__item"><a href="https://www.youtube.com/"
-                                    class="social-links__link"><i class="icon fa fa-youtube"></i></a>
-                            </li>
-                            <li class="social-links__item"><a href="https://www.instgram.com/"
-                                    class="social-links__link"><i class="icon fa fa-instagram"></i></a>
+                            <li class="social-links__item"><a href="https://www.twitter.com" class="social-links__link"><i class="icon fa fa-twitter"></i></a></li>
+                            <li class="social-links__item"><a href="https://www.facebook.com/Speed.courier.service" class="social-links__link"><i class="icon fa fa-facebook"></i></a></li>
+                            <li class="social-links__item"><a href="#" class="social-links__link"><i class="icon fa fa-google-plus"></i></a> </li>
+                            <li class="social-links__item"><a href="https://www.youtube.com/" class="social-links__link"><i class="icon fa fa-youtube"></i></a></li>
+                            <li class="social-links__item"><a href="https://www.instgram.com/" class="social-links__link"><i class="icon fa fa-instagram"></i></a>
                             </li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-sm-5 text-center">
-                    <div class="header-contacts"><a href="#!"><i
-                                class="icon fa fa-phone-square"></i>01098026159 </a><a href="#!"><i
-                                cl010ass="icon fa fa-envelope-square"></i>sales@speedshippings.com</a>
+                    <div class="header-contacts"><a href="#!"><i class="icon fa fa-phone-square"></i>01098026159 </a><a href="#!"><i cl010ass="icon fa fa-envelope-square"></i>sales@speedshippings.com</a>
                     </div>
                 </div>
             </div>
@@ -41,16 +30,12 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="header__logo">
-                    <a href="index.html" class="logo"> <img class="img-responsive"
-                            src="{{asset('front/uploads/logo/1491769630.png')}}" alt="Logo" /> </a>
+                    <a href="index.html" class="logo"> <img class="img-responsive" src="{{asset('front/uploads/logo/1491769630.png')}}" alt="Logo" /> </a>
                 </div>
                 <div class="navbar">
                     <div class="yamm">
                         <div class="navbar-header hidden-md hidden-lg hidden-sm">
-                            <button type="button" data-toggle="collapse"
-                                data-target="#navbar-collapse-1" class="navbar-toggle"><span
-                                    class="icon-bar"></span><span class="icon-bar"></span><span
-                                    class="icon-bar"></span> </button>
+                            <button type="button" data-toggle="collapse" data-target="#navbar-collapse-1" class="navbar-toggle"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </button>
                             <a href="#" class="navbar-brand">Menu</a>
                         </div>
                         <nav id="navbar-collapse-1" class="navbar-collapse collapse">
@@ -59,17 +44,14 @@
                                 <li class=""><a href="prices.html">الأسعار</a></li>
                                 <li class="dropdown"><a href="services.html">خدماتنا </a>
                                     <ul role="menu" class="dropdown-menu">
-                                        <li><a href="services/Same-day-deliveryn.html">توصيل نفس
-                                                اليوم</a></li>
-                                        <li><a href="services/money-collection.html">تحصيل الاموال</a>
-                                        </li>
+                                        <li><a href="services/Same-day-deliveryn.html">توصيل نفس اليوم</a></li>
+                                        <li><a href="services/money-collection.html">تحصيل الاموال</a></li>
                                         <li><a href="services/internal-shipping.html">شحن داخلي</a></li>
                                         <li><a href="services/international.html">الشحن الدولي</a></li>
                                         <li><a href="services/fbs.html">تشحن وتغلف من قبل سبيد</a></li>
                                         <li><a href="services/posta.html">خدمة المراسلين</a></li>
                                         <li><a href="services/speed-app.html">Speed App</a></li>
-                                        <li><a href="services/mailing.html">توزيع المراسلات الكمية</a>
-                                        </li>
+                                        <li><a href="services/mailing.html">توزيع المراسلات الكمية</a></li>
                                         <li><a href="services/documents.html">خدمة توصيل المستندات</a>
                                         </li>
                                     </ul>

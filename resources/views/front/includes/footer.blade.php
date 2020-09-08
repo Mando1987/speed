9d18010d1bb696f10569d093d8f702f9ed370785
Mahmoud Muhammed <mandoooman@gmail.com>
2020-09-08 02:03:54 +0200

    translate footer file

--- resources/views/front/includes/footer.blade.php
+++ resources/views/front/includes/footer.blade.php
@@ -3,21 +3,22 @@
         <div class="row">
             <div class="col-md-4 footer-right">
                 <img class="img-responsive" src="{{asset('front/uploads/logo/1491769630.png')}}" alt="logo" />
-                <p>@lang('front.fotter_title')</p>
+                <p>@lang('front.footer_title')</p>
             </div>
             <div class="col-md-4">
                 <h3>لينكات </h3>
                 <div class="border-color"></div>
                 <ul class="list-mark">
-                    <li><a href="index.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_home')</a></li>
-                    <li><a href="prices.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_prices')</a></li>
-                    <li><a href="makeorders.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_ordercharge') </a>
+                    <li><a href="index.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_home')</a></li>
+                    <li><a href="prices.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_prices')</a></li>
+                    <li><a href="makeorders.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_ordercharge')
+                        </a>
                     </li>
-                    <li><a href="about.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_uswe')</a></li>
-                    <li><a href="contact.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_callme')</a></li>
-                    <li><a href="terms.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_licinse')</a>
+                    <li><a href="about.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_uswe')</a></li>
+                    <li><a href="contact.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_callme')</a></li>
+                    <li><a href="terms.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_licinse')</a>
                     </li>
-                    <li><a href="join.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_jointoteam')</a>
+                    <li><a href="join.html"><i class=" fa fa-angle-left"></i>@lang('front.footer_jointoteam')</a>
                     </li>
                 </ul>
             </div>
@@ -26,20 +27,27 @@
                 <h3>خدماتنا </h3>
                 <div class="border-color"></div>
                 <ul class="list-mark">
-                    <li><a href="services/Same-day-deliveryn.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_sendInTheSameDay')</a>
+                    <li><a href="services/Same-day-deliveryn.html"><i
+                                class=" fa fa-angle-left"></i>@lang('front.footer_sendInTheSameDay')</a>
                     </li>
-                    <li><a href="services/money-collection.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_licinse')</a></li>
-                    <li><a href="services/internal-shipping.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_chargeIN')</a></li>
-                    <li><a href="services/fbs.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_chargeinspeed')</a></li>
-                    <li><a href="services/speed-app.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_speedApp')</a>
+                    <li><a href="services/money-collection.html"><i
+                                class=" fa fa-angle-left"></i>@lang('front.footer_licinse')</a></li>
+                    <li><a href="services/internal-shipping.html"><i
+                                class=" fa fa-angle-left"></i>@lang('front.footer_chargeIN')</a></li>
+                    <li><a href="services/fbs.html"><i
+                                class=" fa fa-angle-left"></i>@lang('front.footer_chargeinspeed')</a></li>
+                    <li><a href="services/speed-app.html"><i
+                                class=" fa fa-angle-left"></i>@lang('front.footer_speedApp')</a>
                     </li>
-                    <li><a href="services/mailing.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_sendQuntitysenders')</a></li>
-                    <li><a href="services/documents.html"><i class=" fa fa-angle-left"></i>@lang('front.fotter_documentSendService')</a></li>
+                    <li><a href="services/mailing.html"><i
+                                class=" fa fa-angle-left"></i>@lang('front.footer_sendQuntitysenders')</a></li>
+                    <li><a href="services/documents.html"><i
+                                class=" fa fa-angle-left"></i>@lang('front.footer_documentSendService')</a></li>
                 </ul>
             </div>
         </div>
         <div class="row">
-            <div class="copyright">@lang('front.fotter_copyright')</div>
+            <div class="copyright">@lang('front.footer_copyright')</div>
         </div>
     </div>
 </footer>

212eb7b73168e878e69d6b37ad82fe8094e773de
AMR-WAHBA <70824114+AMR-WAHBA@users.noreply.github.com>
2020-09-17 19:19:27 +0200

    Update datatable.php

--- resources/lang/ar/datatable.php
+++ resources/lang/ar/datatable.php
@@ -12,16 +12,16 @@ return [
         'active'         => 'متاح',
     ],
     'order' => [
-        'customer'     => 'العميل',
-        'created_at'   => 'التاريخ',
-        'city'         => 'المنطقة',
-        'total_price'  => 'إجمالى الشحنة',
-        'status'       => 'الحالة',
-        'delegate'     => 'المندوب',
+        'customer'       => 'العميل',
+        'created_at'     => 'التاريخ',
+        'city'           => 'المنطقة',
+        'total_price'    => 'إجمالى الشحنة',
+        'status'         => 'الحاله',
+        'delegate'       => 'المندوب',
         //'type'         => 'نوع الشحن',
        // 'price'        => 'قيمة الشحنة',
        // 'charge_price' => 'تكاليف الشحن',
-        'order_num'    => 'رقم البوليصة',
+        'order_num'      => 'رقم البوليصه',
 
     ]
 

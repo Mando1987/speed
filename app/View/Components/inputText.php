a680b3fcc6cefad2867f21d883e67afa1d52850d
Mahmoud Muhammed <mandoooman@gmail.com>
2020-09-21 01:16:17 +0200

    facebook register

--- app/View/Components/inputText.php
+++ app/View/Components/inputText.php
@@ -7,13 +7,13 @@ use Str;
 
 class inputText extends Component
 {
-    public $name, $value, $placeholder , $type, $key = '';
+    public $name, $value, $placeholder , $type , $key = '';
 
-    public function __construct($name , $placeholder = "" , $type = 'text')
+    public function __construct($name ,$value = null ,  $placeholder = "" , $type = 'text')
     {
         $this->name        = $name;
         $this->key         = str_replace('[' , '.' ,str_replace(']' , '' ,$name));
-        // $this->value       = $value ?? '';
+        $this->value       = $value ?? '';
         $this->placeholder = $placeholder;
         $this->type        = $type;
     }

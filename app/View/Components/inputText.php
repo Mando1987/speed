bb78e18c7ec3f2a7ee18ff6df5f596f61c54368b
Mahmoud Muhammed <mandoooman@gmail.com>
2020-09-21 01:53:54 +0200

    facebook register

--- app/View/Components/inputText.php
+++ app/View/Components/inputText.php
@@ -9,14 +9,14 @@ class inputText extends Component
     public $name, $value, $placeHolder , $type;
     public $key;
 
-    public function __construct($name , $value = null ,  $placeHolder = "" , $type = 'text')
+    public function __construct($name , $value = null ,  $placeHolder = '' , $type = 'text' ,$key = '')
     {
         $this->name        = $name;
         $this->key         = \str_replace('[' , '.' ,\str_replace(']' , '' ,$name));
         $this->value       = $value ?? null;
         $this->placeHolder = $placeHolder;
         $this->type        = $type;
-        $this->key         = 123456;
+        $this->key         = $key;
     }
 
     public function render()

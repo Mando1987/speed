cdc1a631fc139ba0a516816c5c7280fd6e9d939f
Mahmoud Muhammed <mandoooman@gmail.com>
2020-09-21 01:21:40 +0200

    facebook register

--- resources/views/components/input-text.blade.php
+++ resources/views/components/input-text.blade.php
@@ -1,4 +1,4 @@
-<input type="{{ $type  }}" name="{{ $name }}" value="{{ $value }}" class="form-control @error($key) is-invalid @enderror"
+<input type="{{ $type  }}" name="{{ $name }}" value="{{ $value ?? null }}" class="form-control @error($key) is-invalid @enderror"
     placeholder="{{ $placeholder ?? '' }}">
 @error($key)
 <span class="error invalid-feedback">{{ $message }}</span>

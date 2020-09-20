b0ae2c80b00695185e4881dad0aa42f3ce175275
Mahmoud Muhammed <mandoooman@gmail.com>
2020-09-20 23:40:09 +0200

    error in inputText

--- app/Http/Controllers/Admin/RegisterController.php
+++ app/Http/Controllers/Admin/RegisterController.php
@@ -2,29 +2,36 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
+
 
 class RegisterController extends Controller
 {
     public function viewRegisterPage()
     {
-         return view('register.register');
+        return view('register.register');
     }
 
     public function register()
     {
-
     }
     public function redirectToFacebook()
     {
         return Socialite::driver('facebook')->with(['access_token' => 'd43b2a56bad09abac57b7b937501b50f'])->redirect();
     }
 
-    public function handleFacebookCallback()
+    public function handleFacebookCallback(Request $request)
     {
         $user = Socialite::driver('facebook')->user();
-
-        return $user->getEmail();
+        $state = $request->get('state');
+        $request->session()->put('state',$state);
+        return $user;
+        $user->getId();
+        $user->getNickname();
+        $user->getName();
+        $user->getEmail();
+        $user->getAvatar();
     }
 }
---
+++ resources/views/register/facebook-register.blade.php
@@ -0,0 +1,113 @@
+@extends('layouts.dashboard')
+
+@section('login')
+<div class="register-box" style="width: 400px !important">
+    <div class="login-logo">
+        <a href="{{ route('admin.index') }}"><b>@lang('site.website_name')</b> @lang('site.website_name_b')</a>
+    </div>
+    <div class="card">
+        <div class="card-body register-card-body">
+            <p class="login-box-msg"></p>
+
+        <form action="{{ route('register') }}" method="post">
+            @csrf
+            @method('POST')
+                <div class="col-md">
+
+                        <div class="form-group row">
+                            <div class="col-sm-4">
+                                <x-label title="{{__('site.phone')}}" />
+                            </div>
+                            <div class="col-sm-8">
+                                <x-input-text name="admin[phone]" type="text"/>
+                            </div>
+                        </div>
+
+                </div>
+                <div class="col-md">
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                            <x-label title="{{__('site.password')}}" />
+                        </div>
+                        <div class="col-sm-8">
+                            <x-input-text type="password" name="password" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                            <x-label title="{{__('site.password_confirmation')}}" />
+                        </div>
+                        <div class="col-sm-8">
+                            <x-input-text type="password" name="password_confirmation" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                            <x-label title="{{__('site.email')}}" />
+                        </div>
+                        <div class="col-sm-8">
+                            <x-input-text type="email" name="admin[email]" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                            <x-label title="{{__('site.governorate')}}" />
+                        </div>
+                        <div class="col-sm-8">
+                            <x-Governorates name="customer[]" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                            <x-label title="{{__('site.city')}}" />
+                        </div>
+                        <div class="col-sm-8">
+                            <x-cities name="customer[]" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                            <x-label title="{{__('site.address')}}" />
+                        </div>
+                        <div class="col-sm-8">
+                            <x-input-text name="customerInfo[address]" type="text"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-8">
+
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-success btn-block">Register</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+
+            <div class="social-auth-links text-center">
+                <p>- OR -</p>
+            <a href="{{ route('facebook.login') }}" class="btn btn-block btn-primary">
+                    <i class="fab fa-facebook mr-2"></i>
+                    @lang('site.facebook_register')
+                </a>
+
+            </div>
+        </div>
+        <!-- /.form-box -->
+    </div><!-- /.card -->
+</div>
+
+@endsection

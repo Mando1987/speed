f97481ef2e9949ea036c9f2074873b1222897c31
Mahmoud Muhammed <mandoooman@gmail.com>
2020-09-20 18:24:28 +0200

    login with facebook

--- app/View/Components/inputText.php
+++ app/View/Components/inputText.php
@@ -9,7 +9,7 @@ class inputText extends Component
 {
     public $name, $value, $placeholder , $type, $key = '';
 
-    public function __construct($name, $value = null, $placeholder = "" , $type = 'text')
+    public function __construct($name, $value = '', $placeholder = "" , $type = 'text')
     {
         $this->name        = $name;
         $this->key         = str_replace('[' , '.' ,str_replace(']' , '' ,$name));

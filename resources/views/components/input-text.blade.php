df05a2d7823f6b753922f55cb1c1dcc7e5065e85
Mahmoud Muhammed <mandoooman@gmail.com>
2020-09-21 02:23:15 +0200

    facebook register

--- resources/views/components/input-text.blade.php
+++ resources/views/components/input-text.blade.php
@@ -3,7 +3,7 @@
 
 
 <input type="{{ $type  }}" name="{{ $name }}" value="{{ $value ?? null }}" class="form-control @error($key ?? $name) is-invalid @enderror"
-    placeholder="{{ $placeholder ?? '' }}">
+    placeholder="{{ $placeholder ?? '' }}" data="{{$key ?? $name}}">
 @error($key ?? $name)
 <span class="error invalid-feedback">{{ $message }}</span>
 @enderror

0f78489c76605c2db489b89b1ce8acaa69e43f86
Mahmoud Muhammed <mandoooman@gmail.com>
2020-09-26 14:07:18 +0200

    finshed customer view orders

--- app/Services/Orders/CustomerOrderFetshDataService.php
+++ app/Services/Orders/CustomerOrderFetshDataService.php
@@ -7,46 +7,9 @@ use App\Services\BaseService;
 
 class CustomerOrderFetshDataService extends BaseService
 {
-    const IMAGE_PATH = 'orders/';
-    private $paginate = 6;
-    private $view = 'list';
-    private $orderStatuses = [
-        'under_review',
-        'under_preparation',
-        'ready_to_chip',
-        'delivered',
-        'postpond',
-        'cancelld',
-    ];
 
     public function handle($request)
     {
-        $this->setView($request->view ?? null);
-        $status = ($request->status ?? false) && in_array($request->status, $this->orderStatuses) ? $request->status : false;
-        $search = $request->search ?? false;
-
-        // dd($search);
-
-        // $orders = Order::select('id', 'reciver_id', 'customer_id', 'created_at', 'status')->with(
-        //     [
-        //         'shipping' => function ($query) {
-        //             $query->selectRaw('id,order_id,charge_on,charge_price,order_num')
-        //                 ->selectRaw('IF(charge_on="sender" ,price - charge_price ,price) AS price');
-        //         },
-
-        //         'reciver' => function ($query) {
-        //             $query->select('id', 'phone', 'fullname', 'city_id');
-        //         },
-        //         'reciver.city',
-
-        //     ]
-        // )->where('customer_id', $request->customer->id)
-
-        //     ->when($status, function ($query, $status) {
-        //         return $query->where('status', $status);
-        //     })
-        //     ->latest()
-        //     ->paginate($this->paginate);
 
         $orders = Order::join('shippings', 'shippings.order_id', '=', 'orders.id')
             ->join('recivers', 'recivers.id', '=', 'orders.reciver_id')
@@ -55,54 +18,32 @@ class CustomerOrderFetshDataService extends BaseService
             ->addSelect('recivers.id', 'recivers.fullname', 'recivers.phone')
             ->selectRaw('shippings.id,shippings.order_id,shippings.charge_on,shippings.charge_price,shippings.order_num')
             ->selectRaw('IF(shippings.charge_on="sender" ,shippings.price - shippings.charge_price ,shippings.price) AS price')
-            ->where('orders.customer_id', $request->customer->id)
-            ->when($status, function ($query, $status) {
-                return $query->where('status', $status);
-            })
-            ->when($search, function ($query, $search) {
-                return $query->where('recivers.fullname', 'LIKE', '%' . $search . '%')
-                    ->orWhere('recivers.phone', 'LIKE', '%' . $search . '%')
-                    ->orWhere('shippings.order_num', 'LIKE', '%' . $search . '%')
-                    ->orWhere('cities.city_name', 'LIKE', '%' . $search . '%')
-                    ->orWhere('cities.city_name_en', 'LIKE', '%' . $search . '%');
+
+            ->where(function ($query) use ($request) {
+                return $query->where('orders.customer_id', $request->customer->id)
+                    ->when($request->search, function ($qsearch) use ($request) {
+                        return $qsearch->where('recivers.fullname', 'LIKE', '%' . $request->search . '%')
+                            ->orWhere('recivers.phone', 'LIKE', '%' . $request->search . '%')
+                            ->orWhere('shippings.order_num', 'LIKE', '%' . $request->search . '%')
+                            ->orWhere('cities.city_name', 'LIKE', '%' . $request->search . '%')
+                            ->orWhere('cities.city_name_en', 'LIKE', '%' . $request->search . '%');
+                    })
+                    ->when($request->status, function ($qstatus) use ($request) {
+                        return $qstatus->where('status', $request->status);
+                    });
             })
             ->latest()
-            ->paginate($this->paginate);
-
-
-        // return $orders;
-
-
+            ->paginate($request->paginate);
 
         return view(
             'order.index.customer',
             [
                 'orders' => $orders,
-                'view'   => $this->view,
-                'status' => $status ?? 'all',
-                'search' => $search
+                'view'   => $request->view,
+                'status' => $request->status ?? 'all',
+                'search' => $request->search
             ]
         );
     }
-    public function setView($value = null)
-    {
-        if ($value == null && session('view')) {
-            $this->view = session('view');
-        } else {
-            if ($value == 'grid' || $value == 'list') {
-                $this->view = $value;
-                session(['view' => $this->view]);
-            }
-        }
-        $this->setPaginate();
-        return $this;
-    }
 
-    private function setPaginate()
-    {
-        $this->paginate = 6;
-        if ($this->view == 'list') {
-            $this->paginate = 12;
-        }
-    }
 }
--- app/Services/Orders/OrdersFetshDataService.php
+++ app/Services/Orders/OrdersFetshDataService.php
@@ -10,24 +10,64 @@ class OrdersFetshDataService extends BaseService
     private $admin;
     private $type;
     private $identifyOrdersFetch;
+    private $orderStatuses = [
+        'under_review',
+        'under_preparation',
+        'ready_to_chip',
+        'delivered',
+        'postpond',
+        'cancelld',
+    ];
+    private $paginate = 6;
+    private $view = 'list';
 
     public function __construct()
     {
         $identify      = auth('admin')->user();
         $this->type    = $identify->type;
         $type          = $this->type;
-        $className     =  __NAMESPACE__ .'\\' . Str::ucfirst($type . 'OrderFetshDataService');
+        $className     =  __NAMESPACE__ . '\\' . Str::ucfirst($type . 'OrderFetshDataService');
         $this->admin   = $identify->$type;
         $this->identifyOrdersFetch = (new $className);
     }
 
     public function handle($request)
     {
+        $this->setView($request->view ?? null);
+
         return $this->identifyOrdersFetch->handle(
-            (object) array_merge($request->all() ,[$this->type => $this->admin])
+            (object) array_merge(
+                $request->all(),
+                [
+                    $this->type => $this->admin,
+                    'status'   => ($request->status ?? false) && in_array($request->status, $this->orderStatuses) ? $request->status : false,
+                    'search'   => $request->search ?? false,
+                    'view'     => $this->view,
+                    'paginate' => $this->paginate,
+                ]
+            )
         );
     }
-
+    private function setView($value = null)
+    {
+        if ($value == null && session('view')) {
+            $this->view = session('view');
+        } else {
+            if ($value == 'grid' || $value == 'list') {
+                $this->view = $value;
+                session(['view' => $this->view]);
+            }
+        }
+        $this->setPaginate();
+        return $this;
+    }
+    private function setPaginate()
+    {
+        $this->paginate = 6;
+        if ($this->view == 'list') {
+            $this->paginate = 12;
+        }
+    }
     // public function editCityPriceRow($id)
     // {
     //     $city = $this->city::where('id', $id)->first();

32548da6e62be77b520a4808cdca3e44290135b1
AMR-WAHBA <70824114+AMR-WAHBA@users.noreply.github.com>
2020-10-01 22:59:22 +0200

    Create errors.php

---
+++ resources/lang/en/errors.php
@@ -0,0 +1,6 @@
+<?php 
+
+return [
+    'ability_title'   => 'Error',
+    'ability_content' => 'You do not have access to this page. If you want to access this page, you can contact the principal manager to allow you to do so',
+];

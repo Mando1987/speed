325c5d00207ee1c01a860a084bc0402db621a4eb
AMR-WAHBA <70824114+AMR-WAHBA@users.noreply.github.com>
2020-10-01 22:09:56 +0200

    Update sidebar.php

--- resources/lang/en/sidebar.php
+++ resources/lang/en/sidebar.php
@@ -3,31 +3,70 @@
 
 return [
 
-     'dashboard.index'  => 'Dashbord',
-     'auth.login'       => 'Login',
-     'admin.index'      => 'Admins',
-     'admin.all'        => 'All Admins',
-     'admin.create'     => 'create New Admin',
-     'admin.show'       => 'Profile',
+     'customer.dashboard.index'     => 'Dashbord',
+     'customer.auth.login'          => 'Login',
+     'customer.admin.index'         => 'Admins',
+     'customer.admin.all'           => 'All Admins',
+     'customer.admin.create'        => 'create New Admin',
+     'customer.admin.show'          => 'Profile',
 
-     'customer.index'   => 'All customers',
-     'customer.create'  => 'Add New Customer',
-     'customer.all'     => 'All clients',
+     'customer.customer.index'      => 'Your Data',
+     'customer.customer.create'     => 'Add New Customer',
+     'customer.customer.all'        => 'All customers',
+     'customer.customer.edit'       => 'Edit your Data ',
 
-     'role.index'       => 'Permissions',
-     'role.create'      => 'Add New Group',
-     'role.all'         => 'All Permissions',
-     'place.index'      => 'Areas',
-     'place.all'        => 'All Areas',
-     'place.create'     => 'Add a New Area',
-     'place.prices'     => 'Shipping price',
-     'place.prices_add' => 'Add price Area',
-     'price.index'      => 'Shipping price',
-     'price.create'     => 'Add price area',
-     'price.edit'       => 'Edite Shipping price',
+     'customer.order.index'         => 'Order management',
+     'customer.order.create'        => 'Add a new Order',
+     'customer.order.all'           => 'All order',
 
-     'order.index'      => 'Order Management',
-     'order.create'     => 'Add a New Order',
-     'order.all'        => 'All Orders',
+     'customer.sellersouq.index'    => 'Souq.com seller',
+     'customer.sellersouq.create'   => 'Add a New Order',
+     'customer.sellersouq.all'      => 'All Order',
+
+
+
+     'manager.dashboard.index'      => 'Dashbord',
+  // 'manager.dashboard.index'      => 'Mail box',
+  // 'manager.dashboard.index'      => 'Alerts',
+
+     'manager.customer.index'       => 'All customers',
+     'manager.customer.create'      => 'Add a new customer',
+     'manager.customer.all'         => 'All clients',
+     'manager.customer.edit'        => 'Edit client Data',
+     'manager.customer.edit_data'   => 'Edit your Data',
+
+     'manager.admin.index'          => 'Administrators',
+     'manager.admin.all'            => 'All Managers',
+     'manager.admin.create'         => 'Add a new Manager',
+
+     'manager.delegate.index'       => 'Managing agents',
+     'manager.delegate.create'      => 'Add a new Delegates',
+     'manager.delegate.all'         => 'All Delegates',
+
+     'manager.place.index'          => 'Areas',
+     'manager.place.all'            => 'All Areas',
+     'manager.place.create'         => 'Add a New Area',
+     'manager.place.prices'         => 'Shipping price',
+
+     'manager.place.prices_add'     => 'Add price Area',
+     'manager.price.index'          => 'Shipping price',
+     'manager.price.create'         => 'Add price area',
+     'manager.price.edit'           => 'Edite Shipping price',
+
+     'manager.order.index'          => 'Order Management',
+     'manager.order.create'         => 'Add a New Order',
+     'manager.order.all'            => 'All Orders',
+
+     'manager.sellersouq.index'     => 'Souq.com seller',
+     'manager.sellersouq.create'    => 'Add a New Order',
+     'manager.sellersouq.all'       => 'All Order',
+     'manager.sellersouq.all'       => 'Storage service',
+
+     'manager.role.index'           => 'Permissions',
+   //'manager.dashboard.index'      => 'Payments',
+   //'manager.dashboard.index'      => 'statistics',
+   //'manager.dashboard.index'      => 'archives',
+   //'manager.dashboard.index'      => 'Maintenance',
+   //'manager.dashboard.index'      => 'Settings',
 
 ];
